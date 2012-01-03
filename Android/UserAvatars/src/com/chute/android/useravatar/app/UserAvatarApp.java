b28e77f043e0baf41e7f66e27cd20946de30544a
darko1002001 <darkogrozdanovski@gmail.com>
2012-01-03 19:18:05 +0100

    cleanup

--- Android/UserAvatars/src/com/chute/android/useravatar/app/UserAvatarApp.java
+++ Android/UserAvatars/src/com/chute/android/useravatar/app/UserAvatarApp.java
@@ -1,42 +1,40 @@
 package com.chute.android.useravatar.app;
 
-import com.chute.android.useravatar.R;
-import com.chute.android.useravatar.util.PreferenceUtil;
-import com.darko.imagedownloader.ImageLoader;
-
 import android.app.Application;
 import android.content.Context;
 import android.util.TypedValue;
 
+import com.chute.android.useravatar.R;
+import com.chute.android.useravatar.util.PreferenceUtil;
+import com.darko.imagedownloader.ImageLoader;
+
 public class UserAvatarApp extends Application {
 
-	@SuppressWarnings("unused")
-	private static final String TAG = UserAvatarApp.class.getSimpleName();
+    @SuppressWarnings("unused")
+    private static final String TAG = UserAvatarApp.class.getSimpleName();
 
-	private static ImageLoader createImageLoader(Context context) {
-		ImageLoader imageLoader = new ImageLoader(context,
-				R.drawable.placeholder_image_small);
-		imageLoader.setRequiredSize((int) TypedValue.applyDimension(
-				TypedValue.COMPLEX_UNIT_DIP, 75, context.getResources()
-						.getDisplayMetrics()));
-		return imageLoader;
+    private static ImageLoader createImageLoader(Context context) {
+	ImageLoader imageLoader = new ImageLoader(context, R.drawable.placeholder_image_small);
+	imageLoader.setRequiredSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+		75, context.getResources().getDisplayMetrics()));
+	return imageLoader;
+    }
+
+    private ImageLoader mImageLoader;
+
+    @Override
+    public void onCreate() {
+	super.onCreate();
+	mImageLoader = createImageLoader(this);
+	PreferenceUtil.init(getApplicationContext());
+    }
+
+    @Override
+    public Object getSystemService(String name) {
+	if (ImageLoader.IMAGE_LOADER_SERVICE.equals(name)) {
+	    return mImageLoader;
+	} else {
+	    return super.getSystemService(name);
 	}
-
-	private ImageLoader mImageLoader;
-
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		mImageLoader = createImageLoader(this);
-		PreferenceUtil.init(getApplicationContext());
-	}
-
-	@Override
-	public Object getSystemService(String name) {
-		if (ImageLoader.IMAGE_LOADER_SERVICE.equals(name)) {
-			return mImageLoader;
-		} else {
-			return super.getSystemService(name);
-		}
-	}
+    }
 }
