a00938d446a87f93db1a61719304a2b24afd7904
Olga Lazarova <olahla@gmail.com>
2012-02-16 00:58:46 +0100

    LocalImagePickerTutorial initial commit

---
+++ Android/LocalImagePickerTutorial/src/com/chute/android/localimagepickertutorial/app/LocalImagePickerTutorialActivity.java
@@ -0,0 +1,62 @@
+package com.chute.android.localimagepickertutorial.app;
+
+import java.util.ArrayList;
+
+import com.chute.android.localimagepickertutorial.R;
+import com.chute.android.localimagepickertutorial.intent.ChoosePhotosActivityIntentWrapper;
+import com.chute.sdk.collections.GCLocalAssetCollection;
+import com.chute.sdk.model.GCLocalAssetModel;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LocalImagePickerTutorialActivity extends Activity implements OnClickListener{
+	 @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		Button selectPhotos = (Button) findViewById(R.id.btnSelectPhotos);
+		selectPhotos.setOnClickListener(this);
+	    }
+
+	    @Override
+	    public void onClick(View v) {
+		ChoosePhotosActivityIntentWrapper wrapper = new ChoosePhotosActivityIntentWrapper(this);
+		wrapper.startActivityForResult(this,
+			ChoosePhotosActivityIntentWrapper.ACTIVITY_FOR_RESULT_KEY);
+	    }
+
+	    @Override
+	    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == ChoosePhotosActivityIntentWrapper.ACTIVITY_FOR_RESULT_KEY) {
+		    if (resultCode == RESULT_OK) {
+			ChoosePhotosActivityIntentWrapper wrapper = new ChoosePhotosActivityIntentWrapper(
+				data);
+			GCLocalAssetCollection localAssetCollection = makeGCLocalAssetCollection(wrapper
+				.getAssetPathList());
+			int photosSelected = localAssetCollection.size();
+			Toast.makeText(getApplicationContext(), photosSelected + " photos selected!",
+				Toast.LENGTH_SHORT).show();
+		    } else {
+			Toast.makeText(getApplicationContext(), "No photos selected", Toast.LENGTH_SHORT)
+				.show();
+		    }
+		}
+	    }
+
+	    public GCLocalAssetCollection makeGCLocalAssetCollection(ArrayList<String> gridSelectedFilePath) {
+		GCLocalAssetCollection collection = new GCLocalAssetCollection();
+		GCLocalAssetModel model = new GCLocalAssetModel();
+		for (String filePath : gridSelectedFilePath) {
+		    model.setFile(filePath);
+		    collection.add(model);
+		}
+		return collection;
+	    }
+}
---
+++ Android/LocalImagePickerTutorial/src/com/chute/android/localimagepickertutorial/app/LocalImagePickerTutorialApp.java
@@ -0,0 +1,8 @@
+package com.chute.android.localimagepickertutorial.app;
+
+import com.chute.android.multiimagepicker.app.MultiImagePickerApp;
+
+public class LocalImagePickerTutorialApp extends MultiImagePickerApp {
+
+	public static final String TAG = LocalImagePickerTutorialApp.class.getSimpleName();
+}
