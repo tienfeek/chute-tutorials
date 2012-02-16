8816a9993237e2f2eb84781eafc494ab2d140042
Olga Lazarova <olahla@gmail.com>
2012-02-16 16:18:16 +0100

    LocalImagePickerTutorial screenshots, readme.md

--- Android/LocalImagePickerTutorial/src/com/chute/android/localimagepickertutorial/app/LocalImagePickerTutorialActivity.java
+++ Android/LocalImagePickerTutorial/src/com/chute/android/localimagepickertutorial/app/LocalImagePickerTutorialActivity.java
@@ -3,13 +3,14 @@ package com.chute.android.localimagepickertutorial.app;
 import java.util.ArrayList;
 
 import com.chute.android.localimagepickertutorial.R;
-import com.chute.android.localimagepickertutorial.intent.ChoosePhotosActivityIntentWrapper;
+import com.chute.android.multiimagepicker.intent.ChoosePhotosActivityIntentWrapper;
 import com.chute.sdk.collections.GCLocalAssetCollection;
 import com.chute.sdk.model.GCLocalAssetModel;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
