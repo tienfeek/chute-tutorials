e861b9a42d9442a7dfeee2ab410151b6324390d6
Olga Lazarova <olahla@gmail.com>
2012-02-16 18:47:49 +0100

    ImageGridTutorial initial commit

---
+++ Android/ImageGridTutorial/src/com/chute/android/imagegridtutorial/app/ImageGridTutorialApp.java
@@ -0,0 +1,8 @@
+package com.chute.android.imagegridtutorial.app;
+
+import com.chute.android.imagegrid.app.ImageGridApp;
+
+public class ImageGridTutorialApp extends ImageGridApp {
+
+	public static final String TAG = ImageGridTutorialApp.class.getSimpleName();
+}
