a4f226bc136a613b17cc1150889b213880ba3646
Olga Lazarova <olahla@gmail.com>
2012-04-04 17:58:35 +0200

    added ListingChutesTutorial functionality to GallerylistingTutorial and deleted ListingChutesTutorial

--- Android/ListingChutesTutorial/src/com/chute/android/listingchutestutorial/app/ListingChutesApp.java
+++
@@ -1,8 +0,0 @@
-package com.chute.android.listingchutestutorial.app;
-
-import com.chute.android.gcchutelisting.app.GalleryListingApp;
-
-public class ListingChutesApp extends GalleryListingApp {
-
-	public static final String TAG = ListingChutesApp.class.getSimpleName();
-}
