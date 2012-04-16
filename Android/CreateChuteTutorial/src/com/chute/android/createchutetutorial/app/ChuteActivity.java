58090fcea4d88e86f2a4e64357d472436db22205
Olga Lazarova <olahla@gmail.com>
2012-04-16 16:03:23 +0200

    refactoring

--- Android/CreateChuteTutorial/src/com/chute/android/createchutetutorial/app/ChuteActivity.java
+++ Android/CreateChuteTutorial/src/com/chute/android/createchutetutorial/app/ChuteActivity.java
@@ -10,6 +10,7 @@ import com.chute.sdk.model.GCHttpRequestParameters;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
