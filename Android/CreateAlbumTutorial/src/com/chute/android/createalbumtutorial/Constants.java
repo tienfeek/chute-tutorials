2f257fbc467ea91aaca709cb8948297dc079917d
Olga Lazarova <olahla@gmail.com>
2012-12-27 19:22:44 +0100

    removing tutorial

--- Android/CreateAlbumTutorial/src/com/chute/android/createalbumtutorial/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.chute.android.createalbumtutorial;
-
-public class Constants {
-
-	public static final String TAG = Constants.class.getSimpleName();
-
-	public static final String ALBUM_NAME = "Album name";
-	public static final String ALBUM_ID = "1946";
-}
