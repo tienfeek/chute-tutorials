42eadb442b6f9e00bc188eb4d9f9f63eb996d0f8
Olga Lazarova <olahla@gmail.com>
2012-12-27 19:23:06 +0100

    removing tutorial

--- Android/ImageGridTutorial/src/com/chute/android/imagegridtutorial/app/ImageGridTutorialApp.java
+++
@@ -1,8 +0,0 @@
-package com.chute.android.imagegridtutorial.app;
-
-import com.chute.android.imagegrid.app.ImageGridApp;
-
-public class ImageGridTutorialApp extends ImageGridApp {
-
-	public static final String TAG = ImageGridTutorialApp.class.getSimpleName();
-}
