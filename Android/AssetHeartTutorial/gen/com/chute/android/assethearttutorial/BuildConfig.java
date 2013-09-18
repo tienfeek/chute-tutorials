54c93df6cc8127df37d465d63143590c3be3771f
olga <olahla@gmail.com>
2013-09-18 13:40:07 +0200

    add updated dependency

---
+++ Android/AssetHeartTutorial/gen/com/chute/android/assethearttutorial/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.chute.android.assethearttutorial;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Android/Gallery Listing Tutorial/gen/com/chute/android/chutelistingtutorial/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.chute.android.chutelistingtutorial;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
