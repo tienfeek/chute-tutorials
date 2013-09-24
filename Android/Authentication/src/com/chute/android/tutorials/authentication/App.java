e9f7dc11b340fe1ab122fc19b66462497702a998
olga <olahla@gmail.com>
2013-09-24 13:10:37 +0200

    add copyright

--- Android/Authentication/src/com/chute/android/tutorials/authentication/App.java
+++ Android/Authentication/src/com/chute/android/tutorials/authentication/App.java
@@ -1,3 +1,25 @@
+/**
+ * The MIT License (MIT)
+
+Copyright (c) 2013 Chute
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.chute.android.tutorials.authentication;
 
 import com.chute.sdk.v2.api.Chute;
@@ -5,15 +27,14 @@ import com.chute.sdk.v2.api.authentication.AuthConstants;
 
 import android.app.Application;
 
-public class App extends Application{
-	
-	private static final String CLIENT_SECRET = "0599436c911d8ee27d34d26c2dde73a1a342a8a0e0b20592ef00f90fe1ca5305";
-	private static final String CLIENT_ID = "4f15d1f138ecef6af9000004";
+public class App extends Application {
 
+  private static final String CLIENT_SECRET = "0599436c911d8ee27d34d26c2dde73a1a342a8a0e0b20592ef00f90fe1ca5305";
+  private static final String CLIENT_ID = "4f15d1f138ecef6af9000004";
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		Chute.init(getApplicationContext(), new AuthConstants(CLIENT_ID, CLIENT_SECRET));
-	}
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    Chute.init(getApplicationContext(), new AuthConstants(CLIENT_ID, CLIENT_SECRET));
+  }
 }
--- Android/Authentication/src/com/chute/android/tutorials/authentication/BaseLoginActivity.java
+++ Android/Authentication/src/com/chute/android/tutorials/authentication/BaseLoginActivity.java
@@ -1,3 +1,25 @@
+/**
+ * The MIT License (MIT)
+
+Copyright (c) 2013 Chute
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.chute.android.tutorials.authentication;
 
 import android.app.Activity;
@@ -11,65 +33,64 @@ import com.chute.sdk.v2.api.authentication.AuthenticationFactory;
 import com.chute.sdk.v2.api.authentication.TokenAuthenticationProvider;
 import com.chute.sdk.v2.model.enums.AccountType;
 
-
-
 public abstract class BaseLoginActivity extends Activity {
 
-	// Fill with values from your chute developer account
+  // Fill with values from your chute developer account
 
-	public static final String TAG = BaseLoginActivity.class.getSimpleName();
+  public static final String TAG = BaseLoginActivity.class.getSimpleName();
 
-	private Button facebookLogin;
-	private Button twitterLogin;
+  private Button facebookLogin;
+  private Button twitterLogin;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login_activity);
-		facebookLogin = (Button) findViewById(R.id.btnFacebook);
-		facebookLogin.setTag(AccountType.FACEBOOK);
-		twitterLogin = (Button) findViewById(R.id.btnTwitter);
-		twitterLogin.setTag(AccountType.TWITTER);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.login_activity);
+    facebookLogin = (Button) findViewById(R.id.btnFacebook);
+    facebookLogin.setTag(AccountType.FACEBOOK);
+    twitterLogin = (Button) findViewById(R.id.btnTwitter);
+    twitterLogin.setTag(AccountType.TWITTER);
 
-		OnLoginClickListener loginClickListener = new OnLoginClickListener();
-		facebookLogin.setOnClickListener(loginClickListener);
-		twitterLogin.setOnClickListener(loginClickListener);
+    OnLoginClickListener loginClickListener = new OnLoginClickListener();
+    facebookLogin.setOnClickListener(loginClickListener);
+    twitterLogin.setOnClickListener(loginClickListener);
 
-		if (TokenAuthenticationProvider.getInstance().isTokenValid()) {
-			facebookLogin.setVisibility(View.GONE);
-			twitterLogin.setVisibility(View.GONE);
-			launchMainAppActivity();
-		} else {
-			facebookLogin.setVisibility(View.VISIBLE);
-			twitterLogin.setVisibility(View.VISIBLE);
-		}
-	}
+    if (TokenAuthenticationProvider.getInstance().isTokenValid()) {
+      facebookLogin.setVisibility(View.GONE);
+      twitterLogin.setVisibility(View.GONE);
+      launchMainAppActivity();
+    } else {
+      facebookLogin.setVisibility(View.VISIBLE);
+      twitterLogin.setVisibility(View.VISIBLE);
+    }
+  }
 
-	private class OnLoginClickListener implements OnClickListener {
-		@Override
-		public void onClick(final View v) {
-			final AccountType accountType = (AccountType) v.getTag();
-			launchAuthenticationActivity(accountType);
-		}
-	}
+  private class OnLoginClickListener implements OnClickListener {
 
-	public abstract void launchMainAppActivity();
+    @Override
+    public void onClick(final View v) {
+      final AccountType accountType = (AccountType) v.getTag();
+      launchAuthenticationActivity(accountType);
+    }
+  }
 
-	protected void launchAuthenticationActivity(AccountType accountType) {
-		AuthenticationFactory.getInstance()
-				.startAuthenticationActivity(BaseLoginActivity.this,
-						accountType);
-	}
+  public abstract void launchMainAppActivity();
 
-	@Override
-	protected void onActivityResult(final int requestCode,
-			final int resultCode, final Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		if (resultCode == RESULT_OK) {
-			NotificationUtil.makeToast(getApplicationContext(),
-					R.string.success);
-			launchMainAppActivity();
-			return;
-		}
-	}
+  protected void launchAuthenticationActivity(AccountType accountType) {
+    AuthenticationFactory.getInstance()
+        .startAuthenticationActivity(BaseLoginActivity.this,
+            accountType);
+  }
+
+  @Override
+  protected void onActivityResult(final int requestCode,
+      final int resultCode, final Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (resultCode == RESULT_OK) {
+      NotificationUtil.makeToast(getApplicationContext(),
+          R.string.success);
+      launchMainAppActivity();
+      return;
+    }
+  }
 }
